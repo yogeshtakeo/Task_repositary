5144658a67a25e9824024f3d95a2beca3d23b2d7
Kenneth Ewonde <Kennethewonde@takeo.ai>
2023-04-07 12:57:42 -0500

    javascript practice

---
+++ Task 4/script.js
@@ -0,0 +1,14 @@
+let foo = "hello";
+
+let bar = "world";
+
+console.log(foo + " " + bar);
+let number = 5;
+console.log(number);
+let array = [5, 4, "hello"];
+console.log(array);
+console.log(array[1]);
+let object = { data: 33 };
+console.log(object.data)
+console.log(object["data"])
+console.log(object)
