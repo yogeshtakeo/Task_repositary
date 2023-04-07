ab5455860cf38c6bdcca56ae2ca99f0a2f0e0277
sushilkhanal <khanal.sushil47@gmail.com>
2023-04-07 12:58:16 -0500

    task 4

---
+++ Task4/script.js
@@ -0,0 +1,15 @@
+const myString = "Java Script Hello to Takeo";
+const myNumber = "5";
+const myBoolean = "true";
+const myArray = [1, 2, 3];
+const myObject = { name: "Takeo", age: 20 };
+const addition = myNumber + "1";
+const substraction = myNumber - "2";
+
+console.log("string:", myString);
+console.log("Number", myNumber);
+console.log("Boolean", myBoolean);
+console.log("Array", myArray);
+console.log("Object", myObject);
+console.log("Addition", addition);
+console.log("Substraction", substraction);
