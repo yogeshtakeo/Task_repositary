c6400cfb5f3d6e94c93a79fcc76f06aa272341a1
Bhabishaphuyal6 <96266221+Bhabishaphuyal6@users.noreply.github.com>
2023-04-11 08:17:56 -0700

    Task 5 Done

--- task 5/script.js
+++ task 5/script.js
@@ -1,17 +1,28 @@
-const people = []
-    const Name= textContent.querySelector('#Name').value;
-    const Age = Number.querySelector('#Age').value;
+const people = [
+  { name: "John", age: 32 },
+  { name: "Sarah", age: 27 },
+  { name: "Jane", age: 41 },
+  { name: "Tom", age: 21 },
+  { name: "Mike", age: 18 }
+];
 
-  
-  people.sort((a, b) => a.age - b.age);
-  
-  people.forEach((person) => {
-    const li = document.createElement("li");
-    li.textContent = `${person.name}, ${person.age}`;
-    peopleList.appendChild(li);
-  const AverageAgeBtn = document.querySelector('#find-Age');
-  const totalAge = people.reduce((sum, person) => sum + person.age, 0);
-const avgAge = totalAge / people.length;
-console.log (The average age is(avgAge));
-}
-}
+
+people.sort((a, b) => a.age - b.age);
+
+
+const peopleList = document.createElement("ul");
+people.forEach(person => {
+  const listItem = document.createElement("li");
+  listItem.textContent = `${person.name} (${person.age})`;
+  peopleList.appendChild(listItem);
+});
+
+
+const peopleContainer = document.getElementById("people-container");
+peopleContainer.appendChild(peopleList);
+
+
+const totalAge = people.reduce((acc, person) => acc + person.age, 0);
+const averageAge = Math.round(totalAge / people.length);
+const averageAgeParagraph = document.getElementById("average-age");
+averageAgeParagraph.textContent = `Average age: ${averageAge}`;
