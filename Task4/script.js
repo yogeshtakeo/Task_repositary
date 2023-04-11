ca2d8ae4e600a40c4ace1e157bf876b4b6630c71
Sabin Sapkota <sabinsapkota4123@gmail.com>
2023-04-11 08:38:21 -0500

    Task 4 Completed

---
+++ Task4/script.js
@@ -0,0 +1,19 @@
+let foo = "hello";
+let bar = "world";
+console.log(foo + " " + bar);
+
+let number = 5;
+console.log(number);
+number += 3;
+console.log(number);
+number -= 2;
+console.log(number);
+
+let array = [5, 4, "hello"];
+console.log(array);
+console.log(array[1]);
+
+let object = { data: 33 };
+console.log(object.data);
+console.log(object["data"]);
+console.log(object);
