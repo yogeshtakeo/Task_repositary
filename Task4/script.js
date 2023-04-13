668c319bfd285357779d2f45fa6923206990e22d
sushilkhanal <khanal.sushil47@gmail.com>
2023-04-13 01:05:09 -0500

    Task 7B

--- Task4/script.js
+++ Task4/script.js
@@ -1,10 +1,10 @@
 const myString = "Java Script Hello to Takeo";
-const myNumber = "5";
-const myBoolean = "true";
+const myNumber = 5;
+const myBoolean = true;
 const myArray = [1, 2, 3];
 const myObject = { name: "Takeo", age: 20 };
-const addition = myNumber + "1";
-const substraction = myNumber - "2";
+const addition = myNumber + 1;
+const substraction = myNumber - 2;
 
 console.log("string:", myString);
 console.log("Number", myNumber);
@@ -13,3 +13,5 @@ console.log("Array", myArray);
 console.log("Object", myObject);
 console.log("Addition", addition);
 console.log("Substraction", substraction);
+
+console.log("age", myObject.age);
